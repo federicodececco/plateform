c742968d000d0ded3b8fd98ee3af1bf042b22002
federicodececco <federicodececco1@gmail.com>
2025-07-01 08:47:40 +0200

    init be

---
+++ backend/src/main/java/com/plateform/restfinder/RestfinderApplication.java
@@ -0,0 +1,13 @@
+package com.plateform.restfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfinderApplication.class, args);
+	}
+
+}
